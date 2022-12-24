beef930aa57e5a3f5ec77c1f5c863aca5f687fe5
Junsu <krpjs0508@gmail.com>
2022-12-24 17:35:10 +0900

    package :: buildSrc 클래스 이름 정리

--- presentation/src/main/java/com/gram/presentation/application/GoToOutsideApplication.kt
+++ presentation/src/main/java/com/gram/presentation/application/GoToOutsideApplication.kt
@@ -1,6 +1,8 @@
 package com.gram.presentation.application
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class GoToOutsideApplication : Application() {
 }
