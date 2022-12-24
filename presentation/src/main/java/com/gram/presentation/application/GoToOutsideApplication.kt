5951f85f92235a45052be25f47fb45dd99fe5650
Junsu <krpjs0508@gmail.com>
2022-12-24 22:43:29 +0900

    refactor :: GoToOUtsideApplication의 불필요한 클래스 몸체 제거

--- presentation/src/main/java/com/gram/presentation/application/GoToOutsideApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.gram.presentation.application
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class GoToOutsideApplication : Application() {
-}
