24bd14028e03e5f9fb9ae7fed19955c8096b5dd2
Junsu <krpjs0508@gmail.com>
2022-12-24 22:42:22 +0900

    feat :: TeacherMainActivity 추가

---
+++ presentation/src/main/java/com/gram/presentation/view/teacher/TeacherMainActivity.kt
@@ -0,0 +1,12 @@
+package com.gram.presentation.view.teacher
+
+import com.gram.presentation.R
+import com.gram.presentation.base.BaseActivity
+import com.gram.presentation.databinding.ActivityMainTeacherBinding
+
+class TeacherMainActivity : BaseActivity<ActivityMainTeacherBinding>(
+    R.layout.activity_main_teacher,
+) {
+    override fun initView() {
+    }
+}
