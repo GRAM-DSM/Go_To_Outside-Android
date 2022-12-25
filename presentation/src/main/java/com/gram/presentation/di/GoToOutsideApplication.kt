93e86a1da3a1b704862a35cd025a8346237add17
Junsu <krpjs0508@gmail.com>
2022-12-25 23:13:26 +0900

    feat :: @HiltAndroidApp Application 클래스 지정

---
+++ presentation/src/main/java/com/gram/presentation/di/GoToOutsideApplication.kt
@@ -0,0 +1,10 @@
+package com.gram.presentation.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+//@HiltAndroidApp
+@HiltAndroidApp
+class GoToOutsideApplication : Application() {
+
+}
