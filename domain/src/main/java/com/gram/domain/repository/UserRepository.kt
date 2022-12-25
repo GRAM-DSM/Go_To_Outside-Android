466d1dbf79a1d25df22d79b306cfaaadf44c3b85
Junsu <krpjs0508@gmail.com>
2022-12-25 23:11:14 +0900

    feat :: UserRepository 구현

--- domain/src/main/java/com/gram/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/gram/domain/repository/UserRepository.kt
@@ -1,10 +1,14 @@
 package com.gram.domain.repository
 
+import com.gram.domain.parameter.user.LoginParameter
+
 interface UserRepository {
 
     suspend fun registerAsStudent()
 
     suspend fun registerAsTeacher()
 
-    fun login()
+    fun login(
+        loginParameter: LoginParameter,
+    )
 }
