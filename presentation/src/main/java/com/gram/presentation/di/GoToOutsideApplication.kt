c474b8bd180228ecbafae7f6602bb4542d00e1a0
Junsu <krpjs0508@gmail.com>
2022-12-25 20:17:57 +0900

    feat :: MainActivity 제거 (학생, 선생 MainActivity 추가 예정)

--- presentation/src/main/java/com/gram/presentation/di/GoToOutsideApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.gram.presentation.di
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-import retrofit2.http.Field
-
-@HiltAndroidApp
-class GoToOutsideApplication : Application()
