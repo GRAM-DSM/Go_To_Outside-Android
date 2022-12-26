2ecd46735cb54bd33f953f6f2893201be54512d3
JunJaBoy <krpjs0508@gmail.com>
2022-12-26 15:01:15 +0900

    feat :: UserRepositoryImpl 멤버에 확장함수 적용

--- data/src/main/java/com/gram/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/gram/data/repository/UserRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package com.gram.data.repository
 
-import com.gram.data.remote.datasource.UserDataSource
-import com.gram.data.request.user.LoginRequest
+import com.gram.data.remote.datasource.user.UserDataSource
+import com.gram.data.remote.request.user.LoginRequest
+import com.gram.data.remote.response.user.toLoginEntity
+import com.gram.domain.entity.user.LoginEntity
 import com.gram.domain.parameter.user.LoginParameter
 import com.gram.domain.repository.UserRepository
 import javax.inject.Inject
@@ -19,12 +21,12 @@ class UserRepositoryImpl @Inject constructor(
         //TODO("Not yet implemented")
     }
 
-    override suspend fun login(loginParameter: LoginParameter) {
-        userDataSource.login(
+    override suspend fun login(loginParameter: LoginParameter): LoginEntity {
+        return userDataSource.login(
             LoginRequest(
                 loginParameter.accountId,
                 loginParameter.password,
             )
-        )
+        ).toLoginEntity()
     }
 }
