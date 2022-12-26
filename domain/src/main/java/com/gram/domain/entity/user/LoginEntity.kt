35e9a919c48929e171c1545c0aa8cae8091ba0c0
JunJaBoy <krpjs0508@gmail.com>
2022-12-26 15:02:35 +0900

    feat :: LoginEntity 수정

--- domain/src/main/java/com/gram/domain/entity/user/LoginEntity.kt
+++ domain/src/main/java/com/gram/domain/entity/user/LoginEntity.kt
@@ -1,6 +1,11 @@
 package com.gram.domain.entity.user
 
 data class LoginEntity(
-    val account_id: String,
-    val password: String,
-)
+    val access_token: String,
+    val refresh_token: String,
+    val authority: Authority,
+) {
+    enum class Authority {
+        STUDENT, TEACHER, ;
+    }
+}
