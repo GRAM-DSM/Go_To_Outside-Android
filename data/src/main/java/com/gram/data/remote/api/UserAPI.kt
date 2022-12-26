f1d1d2830ad227134c6d7fc3fea68254847dbf63
Junsu <krpjs0508@gmail.com>
2022-12-26 11:44:14 +0900

    refactor :: Login HTTP 요청 메서드 수정

--- data/src/main/java/com/gram/data/remote/api/UserAPI.kt
+++ data/src/main/java/com/gram/data/remote/api/UserAPI.kt
@@ -3,11 +3,11 @@ package com.gram.data.remote.api
 import com.gram.data.request.user.LoginRequest
 import com.gram.data.response.user.LoginResponse
 import retrofit2.http.Body
-import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface UserAPI {
 
-    @GET("/login")
+    @POST("/login")
     suspend fun login(
         @Body loginRequest: LoginRequest,
     ): LoginResponse
