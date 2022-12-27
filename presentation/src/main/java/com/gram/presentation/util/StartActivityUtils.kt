e8bdcbbf29de52052ed085ef68ded169c9abb20c
JunJaBoy <krpjs0508@gmail.com>
2022-12-27 17:54:10 +0900

    feat :: StartActivityUtils 구현

--- presentation/src/main/java/com/gram/presentation/util/StartActivityUtils.kt
+++ presentation/src/main/java/com/gram/presentation/util/StartActivityUtils.kt
@@ -1,15 +1,43 @@
 package com.gram.presentation.util
 
-import android.content.Context
+import android.app.Activity
 import android.content.Intent
 
-object StartActivityUtils {
-
-    internal fun <T> startActivity(context: Context, to: Class<T>) {
-        context.startActivity(
+internal fun <T> startActivity(
+    activityContext: Activity, to: Class<T>,
+) {
+    activityContext.run {
+        startActivity(
             Intent(
-                context, to,
-            )
+                activityContext, to,
+            ),
         )
     }
+}
+
+internal fun <T> startActivityFinishingCurrentActivity(
+    activityContext: Activity, to: Class<T>,
+) {
+    startActivity(
+        activityContext, to,
+    )
+    activityContext.finish()
+}
+
+internal fun <T> startActivityInvokingFunction(
+    activityContext: Activity, to: Class<T>, block: () -> Any?
+) {
+    block()
+    startActivity(
+        activityContext, to,
+    )
+}
+
+internal fun <T> startActivityFinishingCurrentActivityInvokingFunction(
+    activityContext: Activity, to: Class<T>, block: () -> Unit
+) {
+    block()
+    startActivityFinishingCurrentActivity(
+        activityContext, to,
+    )
 }
