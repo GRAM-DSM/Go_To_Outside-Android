e1b263902b1c0909d4dff39f8beef95c949b3248
JunJaBoy <krpjs0508@gmail.com>
2022-12-27 17:52:57 +0900

    refactor :: BaseActivity가 상속하는 클래스를 AppCompatActivity로 변경 supportFragmentManager를 사용하기 위함!

--- presentation/src/main/java/com/gram/presentation/base/BaseActivity.kt
+++ presentation/src/main/java/com/gram/presentation/base/BaseActivity.kt
@@ -1,14 +1,14 @@
 package com.gram.presentation.base
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
 abstract class BaseActivity<B : ViewDataBinding>(
     @LayoutRes private val layoutId: Int,
-) : ComponentActivity() {
+) : AppCompatActivity() {
 
     protected val binding: B by lazy {
         DataBindingUtil.setContentView(
