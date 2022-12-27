a86f183901206dbbb6b6b1d5eb29664b79875d1d
JunJaBoy <krpjs0508@gmail.com>
2022-12-27 09:34:51 +0900

    feat :: Application 클래스의 SharedPreferences 주입 구현

--- presentation/src/main/java/com/gram/presentation/di/GoToOutsideApplication.kt
+++ presentation/src/main/java/com/gram/presentation/di/GoToOutsideApplication.kt
@@ -1,10 +1,20 @@
 package com.gram.presentation.di
 
 import android.app.Application
+import com.gram.presentation.util.SharedPreferenceNames.DEFAULT_SHARED_PREFERENCES
+import com.gram.presentation.util.SharedPreferencesUtil
 import dagger.hilt.android.HiltAndroidApp
 
 //@HiltAndroidApp
 @HiltAndroidApp
 class GoToOutsideApplication : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+        SharedPreferencesUtil.sharedPreferences = getSharedPreferences(
+            DEFAULT_SHARED_PREFERENCES,
+            MODE_PRIVATE,
+        )
+    }
 }
