c0124be649270f0826e18ffcffecc205d66c4999
JunJaBoy <krpjs0508@gmail.com>
2022-12-28 21:42:58 +0900

    feat :: RegisterActivity 구현

--- presentation/src/main/java/com/gram/presentation/view/register/RegisterActivity.kt
+++ presentation/src/main/java/com/gram/presentation/view/register/RegisterActivity.kt
@@ -1,5 +1,8 @@
 package com.gram.presentation.view.register
 
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.gram.presentation.R
 import com.gram.presentation.base.BaseActivity
 import com.gram.presentation.databinding.ActivityRegisterBinding
 
@@ -8,6 +11,21 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
 ) {
 
     override fun initView() {
+        with(binding) {
 
+            changeFragment()
+            // initiating beginning value
+            includedRegisterHeader.run {
+                tvHeaderPrimary.text = getString(R.string.register_register)
+                tvHeaderSecondary.text = getString(R.string.register_who_are_you_what)
+            }
+            btnRegisterAction.visibility = View.GONE
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(
+            com.gram.presentation.R.id.container_register_label, fragment,
+        ).commitAllowingStateLoss()
     }
 }
