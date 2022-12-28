8f16547399556e1367df9b4603dfc24b23a82b9f
JunJaBoy <krpjs0508@gmail.com>
2022-12-28 15:11:48 +0900

    feat :: RegisterActivity 추가

---
+++ presentation/src/main/java/com/gram/presentation/view/register/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.gram.presentation.view.register
+
+import com.gram.presentation.base.BaseActivity
+import com.gram.presentation.databinding.ActivityRegisterBinding
+
+class RegisterActivity : BaseActivity<ActivityRegisterBinding>(
+    com.gram.presentation.R.layout.activity_register,
+) {
+
+    override fun initView() {
+
+    }
+}
